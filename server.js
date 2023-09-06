6ec4fcf387f20f8fb10a90d05ff221322170194b
Greenfield01 <greenfieldadolphus22@gmail.com>
2023-09-06 09:24:21 +0100

    test

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import router from './routes/index';
+import startServer from './libs/boot';
+import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
 
-const app = express();
-const port = process.env.PORT || 5000;
+const server = express();
 
-app.use(express.json());
-app.use('/', router);
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+export default server;
