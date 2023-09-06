7bc6e4abd137b0210a0fd2d7b302a70a11fa3afe
Greenfield01 <greenfieldadolphus22@gmail.com>
2023-09-06 09:38:27 +0100

    test

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,18 @@
-/* eslint-disable import/no-named-as-default */
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export default class AppController {
-  static getStatus(req, res) {
-    res.status(200).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    });
+class AppController {
+  static async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+    return res.status(200).json({ redis: redisStatus, db: dbStatus });
   }
 
-  static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    return res.status(200).json({ users: usersCount, files: filesCount });
   }
 }
+
+export default AppController;
